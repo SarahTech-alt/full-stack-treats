cbb083f9d840cffe6b3f31de9a291da0903675eb
Sarah Fuoss <sfuoss@gmail.com>
2021-09-13 13:33:12 -0500

    Created a GET request on the router.

--- server/routes/treats.router.js
+++ server/routes/treats.router.js
@@ -4,6 +4,19 @@ const pool = require('../modules/pool');
 
 // GET /treats
 
+router.get('/', (req,res) => {
+    // text to send to database
+    const queryText = `SELECT * FROM "treats" LIMIT 100;`
+    // input the query text into the database and send 
+    // results back to client if successful, else send error status code
+    pool.query(queryText).then(result => {
+        res.send(results.rows)
+    }).catch(error => {
+        console.log('There was an error getting treats', error);
+        res.sendStatus(500);
+    })
+})
+
 // POST /treats
 
 // PUT /treats/<id>
